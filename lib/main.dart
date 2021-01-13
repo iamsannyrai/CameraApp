0bf7f741622c4b469aa4709666f4ca74a671b504
office <iam.sannyrai@gmail.com>
2021-01-13 10:01:16 +0545

    initializes project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CameraApp());
+}
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
