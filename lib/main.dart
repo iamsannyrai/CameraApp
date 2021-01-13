c95f75008516c5a9e6bd1ac26b5128ef0a6522f0
office <iam.sannyrai@gmail.com>
2021-01-13 10:13:31 +0545

    adds camera package and list all the available cameras

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(CameraApp());
+Future<void> main() async {
+  // Ensures that plugin services are initialized before 'runApp()'
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // lists all the available cameras
+  final cameras = await availableCameras();
+  debugPrint('list of camera: $cameras');
+
+  // picks up the first one
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: CameraApp(camera: firstCamera),
+    ),
+  );
 }
 
 class CameraApp extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraApp({@required this.camera});
+
   @override
   _CameraAppState createState() => _CameraAppState();
 }
